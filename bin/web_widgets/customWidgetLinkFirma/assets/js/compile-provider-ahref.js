5e558ed4ab85bb4fef89cd60f337478fbe559b84
Luis Alberto Sarmiento Cuenca <52260850+Allen2316@users.noreply.github.com>
2023-07-13 04:15:54 -0500

    fixed untracked files

--- bin/web_widgets/customWidgetLinkFirma/assets/js/compile-provider-ahref.js
+++
@@ -1,7 +0,0 @@
-/**
- * Permite registrar el protocolo firmaec en el navegador web Chrome, Firefox, Edge,... * 
- */
-angular.module('bonitasoft.ui').config(function ($compileProvider) {
-    // console.info('Se procede a registrar el protocolo firmaec para que angular no lo marque como: unsafed');
-    $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|firmaec):/)
-})
